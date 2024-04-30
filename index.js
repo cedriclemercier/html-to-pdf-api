7097f9d688d66edd309ec70d65c56a83dc1ecf61
cedriclemercier <info@cedricdesigns.com.au>
2024-04-30 15:40:58 +0700

    Port change

--- index.js
+++ index.js
@@ -115,6 +115,6 @@ app.post('/make-pdf', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
