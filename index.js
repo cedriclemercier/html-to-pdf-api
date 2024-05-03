70497124d6bc07f0499c1cbca14977637bc1f2f5
cedriclemercier <info@cedricdesigns.com.au>
2024-05-03 12:38:09 +0700

    Added image replacer for cover page

--- index.js
+++ index.js
@@ -19,6 +19,44 @@ app.use(upload.any());
 // Create an EventEmitter instance
 const eventEmitter = new EventEmitter();
 
+// ================================================ helper functions =======================================================
+function getRandomImage(images) {
+  const filteredImages = images.filter(
+    (image) => !image.endsWith(".ttf") && !image.endsWith(".svg")
+  );
+
+  let randomIndex = Math.floor(Math.random() * filteredImages.length);
+  while (filteredImages[randomIndex].includes('bg') || filteredImages[randomIndex].includes('background') || filteredImages[randomIndex].includes('logo')) {
+    console.log(`Index has bg or background in name, picking another!`);
+    randomIndex = Math.floor(Math.random() * filteredImages.length);
+  }
+
+  return filteredImages[randomIndex];
+}
+function replaceImagePlaceholder(str, imageFiles) {
+  return str.replace(/\{image\}/g, function () {
+    // Select a random image from imageFiles
+    return getRandomImage(imageFiles);
+  });
+}
+
+function replaceImgToBase64(htmlString, imageFiles, files) {
+  let content = htmlString;
+  for (const file of files) {
+    if (file.fieldname.endsWith('.jpg') || file.fieldname.endsWith('.png')) {
+      const imageBase64 = `data:image/${path.extname(file.fieldname).substring(1)};base64,${file.buffer.toString('base64')}`;
+      // Replace background-image: url() values with base64
+      content = content.replace(new RegExp(`url\\(['"]?${file.fieldname}['"]?\\)`, 'g'), `url('${imageBase64}')`);
+      // Replace img src values with base64
+      content = content.replace(new RegExp(`src=['"]?${file.fieldname}['"]?`, 'g'), `src="${imageBase64}"`);
+    }
+  }
+  return content;
+}
+// =======================================================================================================
+
+
+
 // Endpoint to subscribe to logs via SSE
 app.get('/logs', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
@@ -105,8 +143,10 @@ app.post('/make-pdf', async (req, res) => {
 
       // Take care of replacing images with base64
       logToUI(`Replacing images...`);
+      let imagesArray = []
       for (const file of req.files) {
         if (file.fieldname.endsWith('.jpg') || file.fieldname.endsWith('.png')) {
+          imagesArray.push(file.fieldname)
           // Read image file content as base64
           const imageBase64 = `data:image/${path.extname(file.fieldname).substring(1)};base64,${file.buffer.toString('base64')}`;
           // Replace background-image: url() values with base64
@@ -115,6 +155,10 @@ app.post('/make-pdf', async (req, res) => {
           pageHtmlString = pageHtmlString.replace(new RegExp(`src=['"]?${file.fieldname}['"]?`, 'g'), `src="${imageBase64}"`);
         }
       }
+
+      // Replace cover page image. TODO: All in one code with the lines above
+      pageHtmlString = replaceImagePlaceholder(pageHtmlString, imagesArray);
+      pageHtmlString = replaceImgToBase64(pageHtmlString, imagesArray, req.files)
 
       await page.setContent(pageHtmlString);
       const pdfBuffer = await page.pdf({
@@ -130,9 +174,11 @@ app.post('/make-pdf', async (req, res) => {
       });
 
       // Save the PDF file (optional)
-      // console.log(`Saving to files: output.html and output.pdf !`);
-      // fs.writeFileSync('output.html', pageHtmlString);
-      // fs.writeFileSync('output.pdf', pdfBuffer);
+      if (process.env.ENVIRONMENT && process.env.ENVIRONMENT == 'dev') {
+        console.log(`Saving to files: output.html and output.pdf !`);
+        fs.writeFileSync('output.html', pageHtmlString);
+        fs.writeFileSync('output.pdf', pdfBuffer);
+      }
 
       // Set the content type header to indicate that a PDF blob is being sent in the response
       res.set('Content-Type', 'application/pdf');
